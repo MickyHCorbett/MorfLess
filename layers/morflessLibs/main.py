3dd3d9f03def3551e1aeea6f30c83e72546c4f1d
mickyhcorbett <mhcorbett@corvosastroengineering.com>
2020-01-21 17:28:02 +0000

    Logic for adding template tag modified on this branch

--- layers/morflessLibs/main.py
+++ layers/morflessLibs/main.py
@@ -39,7 +39,7 @@ def polimorf_add_main(main_data,sidebar_data,meta_present,wrap,fileroot,settings
 
         if main_data != ct.PCOM_NO_ENTRY:
             add_main = pcom_create_html_from_array(main_data) + template_tag
-        else:
+        elif:
             add_main = template_tag
 
         if sidebar_data != [ct.PCOM_NO_ENTRY]:

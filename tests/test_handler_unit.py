c80cd025dc823470c1fabc6f8b90e185d4ccbcee
mickyhcorbett <mhcorbett@corvosastroengineering.com>
2020-03-11 19:53:22 +0000

    Test handler unit amended - extra cases added

--- tests/test_handler_unit.py
+++ tests/test_handler_unit.py
@@ -1,7 +1,5 @@
 import unittest
 
-# import test values and expected outputs
-# main includes sidebar data
 from unit.test_handler_html_sections import HtmlSectionsHandlerCase
 from unit.test_handler_read_schematic import ReadSchematicHandlerCase
 from unit.test_handler_string_processes_1 import StringProcesses1HandlerCase
@@ -12,6 +10,10 @@ from unit.test_handler_meta_elements import MetaElementsCase
 from mockAws.test_handler_bucket_operations import RenderHtmlBucketOperations
 from mockAws.test_handler_bucket_operations import CreateListPagesBucketOperations
 from mockAws.test_handler_bucket_operations import SearchContentBucketOperations
+
+from mockAws.test_handler_renderHtml_writes import RenderHtmlReadWrites
+from mockAws.test_handler_createListPages_writes import CreateListPagesReadWrites
+from mockAws.test_handler_createListPages_process import CreateListPagesProcess
 
 def suite():
     suite = unittest.TestSuite()

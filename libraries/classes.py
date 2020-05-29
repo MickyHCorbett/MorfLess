5f8085411ef1e0df9da020aa69dba15b83b84353
mickyhcorbett <mhcorbett@corvosastroengineering.com>
2020-05-29 14:42:46 +0100

    Classes.py cleaned up. Pycaches removed

--- libraries/classes.py
+++ libraries/classes.py
@@ -53,7 +53,9 @@ class HtmlOut:
         # set postlist constant - check for search
         if not self.is_search:
             self.postlist_constant_name = 'postlist--' + sp.pcom_get_template_key(self.fileroot,self.site_settings) + '.js'
-            self.pagination_name = 'pagination--' + sp.pcom_get_template_key(self.fileroot,self.site_settings) + '.js'
+            self.pagination_name = ('pagination--'
+                + sp.pcom_get_template_key(self.fileroot,self.site_settings)
+                + '.js')
         else:
             self.postlist_constant_name = 'postlist--' + self.fileroot + '.js'
             self.pagination_name = 'pagination--' + self.fileroot + '.js'
@@ -72,16 +74,28 @@ class HtmlOut:
         self.update_categories()
         self.update_authors()
         # parse file html
-        self.html = \
-        rs.polimorf_process_schematic_sections(self.all_data['processed'],self.all_data['processed_settings'],self.filename, self.fileroot)
+        self.html = rs.polimorf_process_schematic_sections(
+                            data=self.all_data['processed'],
+                            settings=self.all_data['processed_settings'],
+                            filename=self.filename,
+                            fileroot=self.fileroot)
 
     def get_insert_info(self):
         self.convert_to_array()
         self.insert_info = sp.pcom_get_insert_info(self.html_array)
 
     def process_inserts(self):
-        self.html_array,self.log,self.site_settings,self.dependencies = \
-        rs.pcom_process_inserts(self.html_array,self.insert_info,self.log,self.site_settings,self.filename,self.dependencies)
+
+        (self.html_array,
+        self.log,
+        self.site_settings,
+        self.dependencies) = rs.pcom_process_inserts(
+                                html_array=self.html_array,
+                                insert_info=self.insert_info,
+                                outlog=self.log,
+                                site_settings=self.site_settings,
+                                filename=self.filename,
+                                dependencies=self.dependencies)
         self.combine_array()
 
     # process postlist, pagination and post meta html elements
@@ -97,20 +111,36 @@ class HtmlOut:
 
     def process_postlists(self):
         self.postlists_info = sp.pcom_get_postlists_info(self.html_array,self.fileroot)
-        self.html_array,self.log = \
-        sp.pcom_insert_postlist_wrapper(self.html_array,self.postlists_info,self.log)
+
+        (self.html_array,
+        self.log) = sp.pcom_insert_postlist_wrapper(
+                    html_array=self.html_array,
+                    postlist_info=self.postlists_info,
+                    outlog=self.log)
 
     def process_pagination(self):
         if not self.is_template and not self.is_root and not self.meta[ct.PCOM_META_UNLISTED]:
-            self.html_array,self.pagination_info = ls.pcom_get_pagination_info(self.html_array,self.postlist,self.filename,self.fileroot)
+
+            (self.html_array,
+            self.pagination_info) = ls.pcom_get_pagination_info(
+                                        html_array=self.html_array,
+                                        postlist=self.postlist,
+                                        postname=self.filename,
+                                        fileroot=self.fileroot)
 
     def process_content_meta(self):
         self.content_meta_info = sp.pcom_get_content_meta_info(self.html_array)
         post = ls.pcom_find_post(self.postlist,self.filename)
-        self.html_array = \
-        he.pcom_insert_content_meta_data(self.html_array,self.content_meta_info,self.site_settings,self.list_meta,post,self.is_template)
+        self.html_array = he.pcom_insert_content_meta_data(
+                                    html_array=self.html_array,
+                                    content_meta_info=self.content_meta_info,
+                                    settings=self.site_settings,
+                                    list_meta=self.list_meta,
+                                    post=post,
+                                    is_template=self.is_template)
 
     def insert_additions_into_html(self, placement):
+
         args = {'html': self.html,
         'settings': self.all_data['processed_settings'],
         'placement': placement,
@@ -120,17 +150,20 @@ class HtmlOut:
         'pagination': self.pagination_info,
         'is_template': self.is_template,
         'is_search': self.is_search}
-
+        
         default_additions = ''
 
         self.html = sp.pcom_insert_additions_into_html(args)
-        self.html,default_additions = sp.pcom_insert_default_additions_into_html(args['settings'],self.html,placement)
+
+        (self.html,
+        default_additions) = sp.pcom_insert_default_additions_into_html(args['settings'],self.html,placement)
 
         if placement == ct.PCOM_HEADER_PLACEMENT:
             self.site_settings['header_additions'].append(default_additions)
 
         if placement == ct.PCOM_FOOTER_PLACEMENT:
             self.site_settings['footer_additions'].append(default_additions)
+
 
     def get_raw_content(self):
         if not self.is_search and not self.meta[ct.PCOM_META_UNLISTED]:
@@ -142,17 +175,35 @@ class HtmlOut:
         # get type and url from file name
         # only update is meta not NONE
         if self.meta != ct.PCOM_NO_ENTRY and not self.is_template and not self.is_root and not self.meta[ct.PCOM_META_UNLISTED]:
-            df = self.site_settings['date_format']
+
             url,type = sp.pcom_create_url(self.filename,self.meta)
-            self.postlist = ls.pcom_update_postlist(self.postlist,self.filename,url,self.meta,df,type,self.site_settings)
+
+            self.postlist = ls.pcom_update_postlist(
+                                postlist=self.postlist,
+                                postname=self.filename,
+                                url=url,
+                                meta=self.meta,
+                                date_format=self.site_settings['date_format'],
+                                type=type,
+                                settings=self.site_settings)
 
     def update_categories(self):
         if not self.is_template and not self.is_root and not self.meta[ct.PCOM_META_UNLISTED]:
+
             post = ls.pcom_find_post(self.postlist,self.filename)
-            self.list_meta['categories'] = \
-            ls.pcom_update_categories_from_postlist_data(self.list_meta['categories'],post['categories'],self.site_settings,post['type'])
+
+            self.list_meta['categories'] = ls.pcom_update_categories_from_postlist_data(
+                                                    categories=self.list_meta['categories'],
+                                                    cat_list=post['categories'],
+                                                    settings=self.site_settings,
+                                                    type=post['type'])
 
     def update_authors(self):
         if not self.is_template and not self.is_root and not self.meta[ct.PCOM_META_UNLISTED]:
+
             post = ls.pcom_find_post(self.postlist,self.filename)
-            self.list_meta['authors'] = ls.pcom_update_authors_from_postlist_data(self.list_meta['authors'],post['authors'],self.site_settings)
+
+            self.list_meta['authors'] = ls.pcom_update_authors_from_postlist_data(
+                                                    authors=self.list_meta['authors'],
+                                                    author_list=post['authors'],
+                                                    settings=self.site_settings)
